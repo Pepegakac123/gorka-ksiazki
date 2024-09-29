0affe3cd3ce07cb93976bbbf61790bed5a1f3a23
Pepegakac123 <kacpperr.adamczyk@gmail.com>
2024-09-29 16:02:15 +0200

    Dodanie manualnego pobierania wartości z plików .env

--- php_scripts/connect.php
+++ php_scripts/connect.php
@@ -1,9 +1,28 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-    $host=$_ENV['DB_HOST'];
-    $db_user=$_ENV['DB_USER'];
-    $db_password=$_ENV['DB_PASS'];
+function loadEnv($path) {
+    if(!file_exists($path)) {
+        throw new Exception("The environment file {$path} does not exist");
+    }
+    
+    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) {
+            continue;
+        }
+        list($name, $value) = explode('=', $line, 2);
+        $name = trim($name);
+        $value = trim($value);
+        if (!array_key_exists($name, $_ENV)) {
+            putenv(sprintf('%s=%s', $name, $value));
+            $_ENV[$name] = $value;
+        }
+    }
+}
+
+// Load the .env file
+loadEnv(__DIR__ . '/.env');
+    $host=getenv('DB_HOST');
+    $db_user=getenv('DB_USER');
+    $db_password=getenv('DB_PASS');
     $db_name="u780588825_books_mountain"; // tu będzie inna baza
 ?>
